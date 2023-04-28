575de3d820cc3511f490ae1206c8d7f650ffec83
patricia1602 <80828415+patricia1602@users.noreply.github.com>
2023-04-28 11:06:56 -0300

    validacao

--- api/src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ api/src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -1,12 +1,26 @@
 package med.voll.api.endereco;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record DadosEndereco(
-        String logradouro,
-        String bairro,
-        String cep,
-        String cidade,
-        String uf,
-        String complemento,
-        String numero) {
+
+                @NotBlank
+                String logradouro,
+                
+                @NotBlank
+                String bairro,
+                
+                @NotBlank
+                @Pattern(regexp = "\\d{8}")
+                String cep,
+
+                @NotBlank
+                String cidade,
+                
+                @NotBlank
+                String uf,
+                String complemento,
+                String numero) {
 
 }
--- api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,13 +1,31 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
-        String nome,
-        String email,
-        String crm,
-        Especialidade especialidade,
-        DadosEndereco endereco
+
+                @NotBlank 
+                String nome,
+
+                @NotBlank
+                @Email
+                String email,
+                
+                @NotBlank
+                @Pattern(regexp = "\\d{4,6}")  //expressão regular de 4 a 6 digitos
+                String crm,
+                
+                @NotNull
+                Especialidade especialidade,
+                
+                @NotNull
+                @Valid
+                DadosEndereco endereco
 
 ) {
 
