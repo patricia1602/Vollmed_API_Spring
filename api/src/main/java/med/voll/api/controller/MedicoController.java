747ef7f94355c04fc1d7a88aa06467097c9a741c
patricia1602 <80828415+patricia1602@users.noreply.github.com>
2023-05-03 23:27:55 -0300

    atualizar

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,11 +7,13 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import med.voll.api.medico.DadosAtualizacaoMedico;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
@@ -28,10 +30,18 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
-   }
+    }
 
    @GetMapping
-   public Page<DadosListagemMedico> listar(@PageableDefault(page = 0, size = 10, sort = {"nome"}) Pageable paginacao) {
+   public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
     return repository.findAll(paginacao).map(DadosListagemMedico::new);
    }
+
+        @PutMapping
+        @Transactional
+        public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+            var medico = repository.getReferenceById(dados.id());
+            medico.atualizarInformacoes(dados);
+
+        }
 }
